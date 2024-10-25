c9dbc66196522cede7df89089c3f3fcd901f7d72
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-10-24 21:34:32 -0300

    Update Editora.java

--- src/Editora.java
+++ src/Editora.java
@@ -1,17 +1,18 @@
+//importação das bibliotecas
 import java.util.ArrayList;
 import java.util.List;
 
-public class Editora {
-
+public class Editora {//criação da classe editora
+    //variáveis livro, que tem matriz livro e editora tipo string e são privadas
     private List<Livro> livros;
     private String editora;
 
-    public Editora(String editora){
+    public Editora(String editora){//construtor
         this.editora = editora;
         this.livros = new ArrayList<>();
     }
 
-    public void adicionarLivro(Livro livro){
+    public void adicionarLivro(Livro livro){//método
         livros.add(livro);
     }
 }
