fb84b35d348fd99a20634ddf7ed5ea66ba1040c7
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-10-24 21:36:22 -0300

    Update Funcionario.java

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,15 +1,15 @@
-public class Funcionario extends Pessoa{
-
+public class Funcionario extends Pessoa{//criação da classe Funcionario que está herdando da classe Pessoa
+    //variaveis do tipo String, privadas 
     private String matricula;
     private String cargo;
 
-    public Funcionario(String nome, String email, String matricula, String cargo) {
+    public Funcionario(String nome, String email, String matricula, String cargo) {//construtor 
         super(nome, email);
         this.matricula = matricula;
         this.cargo = cargo;
     }
     @Override
-    public void exibirDetalhes(){
+    public void exibirDetalhes(){//método
 
     };
 }
