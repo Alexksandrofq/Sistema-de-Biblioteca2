ad0dda73db7614188f6352cbdcbb8011a0d9c296
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-10-24 21:39:53 -0300

    Update Pessoa.java
    Up Pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,10 +1,11 @@
-abstract class Pessoa {
-    protected  String nome;
+abstract class Pessoa {  // Classe abstrata
+    //Variaveis protegidas, somente subclasses podem utilizar-las
+    protected  String nome; 
     protected  String email;
 
-    public Pessoa(String nome, String email) {
+    public Pessoa(String nome, String email) { //Construtor
         this.nome = nome;
         this.email = email;
     }
-    public abstract  void exibirDetalhes();
+    public abstract  void exibirDetalhes(); //Método
 }
